c7ca0a34bad5f68b3c648139d94b6fc3b6923bd1
Fabio Silva <fabioluiz.silva1@gmail.com>
2020-04-15 09:39:47 -0300

    acrescentada a pergunta sobre como vc esta

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("COmo voce está hoje?");
             Console.ReadLine();
         }
     }
